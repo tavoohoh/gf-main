974bbc890668e1c5f31e3427a9126695b17d2d30
Gustavo Santamaria <gustavo.santamaria@rappi.com>
2019-12-10 14:15:34 -0500

    player layout

--- projects/gs-player/src/lib/gs-player.component.ts
+++ projects/gs-player/src/lib/gs-player.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GsPlayerService } from './gs-player.service';
 
 
