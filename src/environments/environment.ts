f835bf30a81eb75f664e3e3cb4dd67c80c940fd0
Gustavo Santamaria <37514022+tavoohoh@users.noreply.github.com>
2021-09-01 20:08:42 -0500

    remove s data

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,15 +5,15 @@ export const environment = {
   api: {
     functions: testQA ?
       'http://localhost:5000/gianfaraoneweb/us-central1/app/' :
-      'https://us-central1-gianfaraoneweb.cloudfunctions.net/app/'
+      ''
   },
   firebase: {
-    apiKey: 'AIzaSyBBL-0x0RHmgmfRWTpBvHzBQS62mDwbEGQ',
-    authDomain: 'gianfaraoneweb.firebaseapp.com',
-    databaseURL: 'https://gianfaraoneweb.firebaseio.com',
-    projectId: 'gianfaraoneweb',
-    storageBucket: 'gianfaraoneweb.appspot.com',
-    messagingSenderId: '957873330274',
-    appId: '1:957873330274:web:65018019d5fb5ddb26d93b'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: ''
   }
 };
