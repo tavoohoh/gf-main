adaf5f610633544d6446fb7439d789ef4e3816f1
Gustavo Santamaria <37514022+tavoohoh@users.noreply.github.com>
2021-09-01 20:07:51 -0500

    remove fb data

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,16 +1,16 @@
 export const environment = {
   production: true,
   api: {
-    functions: 'https://us-central1-gianfaraoneweb.cloudfunctions.net/app/'
+    functions: ''
   },
   firebase: {
-    apiKey: 'AIzaSyBBL-0x0RHmgmfRWTpBvHzBQS62mDwbEGQ',
-    authDomain: 'gianfaraoneweb.firebaseapp.com',
-    databaseURL: 'https://gianfaraoneweb.firebaseio.com',
-    projectId: 'gianfaraoneweb',
-    storageBucket: 'gianfaraoneweb.appspot.com',
-    messagingSenderId: '957873330274',
-    appId: '1:957873330274:web:65018019d5fb5ddb26d93b'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: ''
   }
 };
 
